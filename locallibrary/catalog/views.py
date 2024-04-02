2fa6405679bc6b6a057769473daa42b55ae74bff
Ligia Schulz <schulzligia@gmail.com>
2024-04-02 10:54:34 -0500

    Added booklist and authorlist views
    Also added detail views for books and authors. Part 6 complete.

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from .models import Book, BookInstance, Author, Genre
+from django.views import generic
 
 # Create your views here.
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 5
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
 
 def index(request):
     """View function for home page of site."""
